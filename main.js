98ebf38cc1c0ec8d062dcba0f32ae3a4a320faa3
Mal <malrsteffes@gmail.com>
2020-10-02 15:53:20 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+// Select our DOM objects
+
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row.seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const selectedMovie = document.getElementById("movie");
+const ticket_price = +selectedMovie.value;
+
+// Grab the container and add the Event Listener onto it
+container.addEventListener("click", e => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+  }
+});
